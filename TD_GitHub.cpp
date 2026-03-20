fee0d6fc55263989859e284df5ad3ede1294e6cd
soliman3333 <sorenvaldemare@gmail.com>
2026-03-20 09:57:34 +0100

    Update TD_GitHub.cpp
    version 2 étudiant 1

--- TD_GitHub.cpp
+++ TD_GitHub.cpp
@@ -1,12 +1,55 @@
 //Fonctions réalisées par l'étudiant 1 :
-int estMajeur (int )
+bool estMajeur(int age)
+{
+    if(age >= 18)
+    {
+        return true;
+    }
+    else
+    { 
+        return false;
+    }
+}
 
+//Calcul du nombre exp 
+int exposant(int nombre, int exp)
+{
+    int resultat = 1;
+    for(int i = 0; i < exp; i++)
+    {
+        resultat = resultat * nombre;
+    }
+    return resultat;
+}
 
+// Calcul pour la TVA
+float TVA(int prix)
+{
+    if(prix >= 1000)
+    {
+        return prix * 1.196; //TVA à 19.6%
+    }
+    else
+    {
+        return prix * 1.055; //TVA de 5.5%
+    }
+}
 
+// Plus grand des 3 nombres
+int plusGrand(int a, int b, int c)
+{
+    int max = a;
 
-
-
-
+    if(b > max)
+    {
+        max = b;
+    }
+    if(c > max)
+    {
+        max = c;
+    }
+    return max;
+}
 
 
 //Fonctions réalisées par l'étudiant 2 :
